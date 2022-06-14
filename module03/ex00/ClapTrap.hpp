3efa495618179396cc099909e744df2fe484e83c
Citrus <contact.citrus@protonmail.com>
2022-06-14 16:21:55 +0200

    mod03 ex00 done

--- module03/ex00/ClapTrap.hpp
+++ module03/ex00/ClapTrap.hpp
@@ -8,12 +8,16 @@ public:
 
 private:
 	std::string _name;
-	unsigned int _hitPoints;
-	unsigned int _energyPoints;
-	unsigned int _attackDamage;
+	int _hitPoints;
+	int _energyPoints;
+	int _attackDamage;
 
 public:
 	void attack(const std::string& target);
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
+	void status();
+
+private:
+	int checkRessources();
 };
--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
+	ClapTrap jeff("Jeff");
+	ClapTrap alice("Alice");
 
+	jeff.attack("Alice");
+	alice.takeDamage(4);
+	alice.beRepaired(2);
+	jeff.takeDamage(8);
+	jeff.status();
+	alice.status();
 }
