c2fed6dd278b0018ab077742fce7f5f63723b805
Citrus <contact.citrus@protonmail.com>
2023-01-16 09:55:09 +0100

    push debug version for day06/ex00

--- day06/ex00/parse.cpp
+++ day06/ex00/parse.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+#define NPOS std::string::npos
+
+bool is_number(const std::string& s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
+}
+
 bool strischar(std::string str)
 {
 	if ((int)str.length() == 1) // char are by definition one character long
@@ -23,73 +32,82 @@ bool strisint(std::string str)
 
 bool strisfloat(std::string str)
 {
-	if (str == "nanf" || str == "inff" || str == "-inff") // Float specifics
+	size_t start = str.find_first_of('-');
+	size_t point = str.find_first_of('.');
+	size_t fpos = str.find_first_of('f');
+	
+	// Compare to float-specific values
+	if (str == "nanf" || str == "inff" || str == "-inff")
 		return true;
-	bool isnegative = false;
-	bool isdigit = true;
-	int i = -1;
-	if (str[0] == '-')
-	{
-		isnegative = true;
-		i++;
-	}
-	while (isdigit && ++i < (int)str.length()) // Iterate over string
-	{
-		if (str[i] < '0' || str[0] > '9') // Look for non-digit chars
-			isdigit = false;
-	}
-	std::cout << "ratio" << std::endl;
-	if (i >= (int)str.length() - 1)
-		return false;
-	std::cout << "out: " << str[i] << i << std::endl;
-	if (str[i] == '.' && ((i >= 1 && !isnegative) || (i >= 2 && isnegative)))
-	{
-		isdigit = true;
-		std::cout << "test" << std::endl;
-		while (isdigit && ++i < (int)str.length()) // Iterate over string
-		{
-			std::cout << str[i];
-			if (str[i] < '0' || str[0] > '9') // Look for non-digit chars
-				isdigit = false;
-		}
-		std::cout << std::endl;
-		std::cout << "allo: " << str[i] << i << std::endl;
-		if (i == (int)str.length())
-			return false;
-		std::cout << "allo2" << std::endl;
+	// std::cerr << "No specific" << std::endl;
 
-		if (str[i] == 'f' && i == (int)str.length())
-			return true;
-	}
-	return false;
+	// Check if there is no point(.) or float char(f)
+	if (point == NPOS || fpos == NPOS)
+		return false;
+	// std::cerr << "Point and float identifier found" << std::endl;
+
+	// Check if multiple/invalid minus sign(s) (invalid: not str[0])
+	if (start != str.find_last_of('-') || (start != NPOS && start != 0))
+		return false;
+	start = (start == NPOS) ? 0 : start + 1;
+	// std::cerr << "Valid/No minus sign: " << str.substr(start, point - start) << std::endl;
+
+	// Check if everything up to '.' is digit(s)
+	if (!is_number(str.substr(start, point - start)))
+		return false;
+	// std::cerr << "Start to point is digit" << std::endl;
+	
+	// Check if there are multiple '.'
+	if (point != str.find_last_of('.'))
+		return false;
+	point++;
+	//std::cerr << "Single point : " << str.substr(point + 1, fpos - point) << std::endl;
+
+	// Check if everything up to 'f' is digit(s)
+	if (!is_number(str.substr(point, fpos - point)))
+		return false;
+	// std::cerr << "Point to f is digit" << std::endl;
+
+	// Check if there are multiple/invalid 'f' (invalid: not str.last)
+	if (fpos != str.find_last_of('f') || fpos != str.length() - 1)
+		return false;
+	// std::cerr << "Valid/Single float identifier" << std::endl;
+
+	// All checks passed, it's a float!
+	return true;
 }
 
 bool strisdouble(std::string str)
 {
-	bool isnegative = false;
-	bool isdigit = true;
-	int i = -1;
-	if (str[0] == '-')
-	{
-		isnegative = true;
-		i++;
-	}
-	while (isdigit && ++i < (int)str.length()) // Iterate over string
-	{
-		if (str[i] < '0' || str[0] > '9') // Look for non-digit chars
-			isdigit = false;
-	}
-	if (i >= (int)str.length() - 1)
+	size_t start = str.find_first_of('-');
+	size_t point = str.find_first_of('.');
+	
+	// Compare to double-specific values
+	if (str == "nan" || str == "inf" || str == "-inf")
+		return true;
+
+	// Check if there is no point(.)
+	if (point == NPOS)
 		return false;
-	if (str[i] == '.' && ((i > 1 && !isnegative) || (i > 2 && isnegative)))
-	{
-		isdigit = true;
-		while (isdigit && ++i < (int)str.length()) // Iterate over string
-		{
-			if (str[i] < '0' || str[0] > '9') // Look for non-digit chars
-				isdigit = false;
-		}
-		return isdigit;
-	}
-	return false;
+
+	// Check if multiple/invalid minus sign(s) (invalid: not str[0])
+	if (start != str.find_last_of('-') || (start != NPOS && start != 0))
+		return false;
+	start = (start == NPOS) ? 0 : start + 1;
+
+	// Check if everything up to '.' is digit(s)
+	if (!is_number(str.substr(start, point - start)))
+		return false;
+	
+	// Check if there are multiple '.'
+	if (point != str.find_last_of('.'))
+		return false;
+	point++;
+
+	// Check if everything up to end is digit(s)
+	if (!is_number(str.substr(point)))
+		return false;
+
+	// All checks passed, it's a double!
+	return true;
 }
